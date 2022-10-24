ab39e7b81e063c2de2e951ae350618c2c480e286
LisaCheung <cheung.lisa111@gmail.com>
2022-10-24 13:55:57 -0700

    react setup

---
+++ collegecommerce-client/src/components/Error.js
@@ -0,0 +1,12 @@
+function Error() {
+    return (
+        <div>
+            <h3>Whoops.</h3>
+            <div className="alert alert-danger">
+                An unspecified error occurred.
+            </div>
+        </div>
+    );
+}
+
+export default Error;
