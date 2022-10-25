a753c101fe527e4f09d13792f5994ff9131e46ea
Dylan Martie <dylanmartie@hotmail.com>
2022-10-25 15:33:02 -0500

    Added Security files, AuthController

--- collegecommerce-server/src/main/java/learn/capstone/AppConfig.java
+++ collegecommerce-server/src/main/java/learn/capstone/AppConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +13,19 @@ public class AppConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        // Configures CORS globally instead of per controller
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
--- collegecommerce-server/src/test/java/learn/capstone/domain/ItemServiceTest.java
+++ collegecommerce-server/src/test/java/learn/capstone/domain/ItemServiceTest.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class ItemServiceTest {
     @Autowired
     ItemService service;
