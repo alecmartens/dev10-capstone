d5f15570f1ce6c63373e9bde25ae4a77c806825c
Alec Martens <am400316@ohio.edu>
2022-10-27 11:37:52 -0400

    Able to view and add items

--- collegecommerce-client/src/components/item/ItemForm.js
+++ collegecommerce-client/src/components/item/ItemForm.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
-import { findById,findAll, save } from "../../services/itemService";
+import { findById, findAll, save } from "../../services/itemService";
 function ItemForm() {
     const [item, setItem] = useState({
         itemId: 0,
-        name:"", 
-        price:0, 
-        description:"", 
-        item_condition:"",
-        item_sold:false,
-        category:"",
-        image_url:""
+        name: "",
+        price: 0,
+        description: "",
+        itemCondition: "",
+        itemSold: false,
+        category: "",
+        imageUrl: ""
     });
     const [errs, setErrs] = useState([]);
     const history = useHistory();
@@ -22,6 +22,7 @@ function ItemForm() {
                 .catch(() => history.push("/invalid"));
         }
     }, []);
+
     function handleChange(evt) {
         const nextItem = { ...item };
         nextItem[evt.target.name] = evt.target.value;
@@ -40,7 +41,7 @@ function ItemForm() {
                 }
             });
     }
-    return(
+    return (
         <form onSubmit={handleSubmit}>
             <h2>{id > 0 ? "Edit item" : "Add item"}</h2>
             <div className="mb-3">
@@ -50,24 +51,32 @@ function ItemForm() {
             </div>
             <div className="mb-3">
                 <label htmlFor="price" className="form-label">Price</label>
-                <input type="text" name="price" id="price" className="form-control"
+                <input type="number" name="price" id="price" className="form-control"
                     value={item.price} onChange={handleChange} />
             </div>
             <div className="mb-3">
                 <label htmlFor="description" className="form-label">Description</label>
                 <input type="text" name="description" id="description" className="form-control"
-                    value={item.name} onChange={handleChange} />
+                    value={item.description} onChange={handleChange} />
             </div>
             <div className="mb-3">
                 <label htmlFor="itemCondition" className="form-label">Item Condition</label>
                 <input type="text" name="itemCondition" id="itemCondition" className="form-control"
                     value={item.itemCondition} onChange={handleChange} />
             </div>
-            <div className="mb-3">
-                <label htmlFor="itemSold" className="form-label">Item Sold</label>
-                <input type="text" name="itemSold" id="itemSold" className="form-control"
-                    value={item.itemSold} onChange={handleChange} />
-            </div>
+            <fieldset>
+                <legend>Item Sold</legend>
+                <div className="mb-3">
+                    <label htmlFor="itemSold" className="form-label">True</label>
+                    <input type="radio" id="itemSold" name="itemSold"
+                        value={item.itemSold} onChange={handleChange}></input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="itemSold" className="form-label">False</label>
+                    <input type="radio" id="itemSold" name="itemSold"
+                        value={item.itemSold} onChange={handleChange}></input>
+                </div>
+            </fieldset>
             <div className="mb-3">
                 <label htmlFor="category" className="form-label">Category</label>
                 <input type="text" name="category" id="category" className="form-control"
@@ -78,17 +87,19 @@ function ItemForm() {
                 <input type="text" name="imageUrl" id="imageUrl" className="form-control"
                     value={item.imageUrl} onChange={handleChange} />
             </div>
-            {errs.length !== 0 && <div className="alert alert-danger">
-                <ul>
-                    {errs.map(err => <li key={err}>{err}</li>)}
-                </ul>
-            </div>}
+            {
+                errs.length !== 0 && <div className="alert alert-danger">
+                    <ul>
+                        {errs.map(err => <li key={err}>{err}</li>)}
+                    </ul>
+                </div>
+            }
             <div className="mb-3">
                 <button className="btn btn-primary me-2" type="submit">Save</button>
                 <Link to="/items" className="btn btn-warning">Cancel</Link>
             </div>
-        </form>
-    ); 
+        </form >
+    );
 }
 
 export default ItemForm;
