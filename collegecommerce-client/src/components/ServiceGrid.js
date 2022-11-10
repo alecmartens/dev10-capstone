9919a841f4cfdd026605d187a2c5499f14942b0f
Alec Martens <am400316@ohio.edu>
2022-11-10 11:31:23 -0500

    Cleaned up front end

--- collegecommerce-client/src/components/ServiceGrid.js
+++ collegecommerce-client/src/components/ServiceGrid.js
@@ -59,9 +59,6 @@ function ServiceGrid({ handleEdit, handleDelete }) {
             <div className="row">
                 <h1 className="col-9">services</h1>
                 <div className="col-3">
-                    {/* <Link to="/cart" className="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
-                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                    </svg><Badge bg="secondary">{count}</Badge></Link> */}
                     <Link to="/services/add" className="btn btn-primary">Add a service</Link>
                 </div>
             </div>
@@ -104,66 +101,12 @@ function ServiceGrid({ handleEdit, handleDelete }) {
                                             id="switch"
                                             label="" ref={ref} onChange={handleChange(s)}
                                         />}</Form></td>
-                                {/* <td><Link to={`/services/delete/${s.serviceId}`} className="btn btn-danger me-2">Delete</Link></td> */}
-                                {/* <td><Link to={`/services/edit/${s.serviceId}`} className="btn btn-secondary">Edit</Link></td> */}
                                 <td><Link to={`/services/delete/${s.serviceId}`} className="btn btn-danger m-2">Delete</Link>
                                     <Link to={`/services/edit/${s.serviceId}`} className="btn btn-secondary m-2">Edit</Link>
-                                    {/* <button className="btn btn-primary" onClick={() => {
-                                        if (!localStorage.getItem("cartProducts")) { localStorage.setItem("cartProducts", JSON.stringify({})); };
-                                        setCount(count + 1);
-                                        setShow(true);
-                                        (localStorage.getItem("cartCount")) ? localStorage.setItem("cartCount", parseInt(localStorage.getItem("cartCount")) + 1) : localStorage.setItem("cartCount", 1);
-                                        let cart = JSON.parse(localStorage.getItem("cartProducts"));
-                                        if (cart[service.serviceId]) {
-                                            cart[service.serviceId] += 1;
-                                        }
-                                        else {
-                                            cart[service.serviceId] = 1;
-                                        }
-                                        localStorage.setItem("cartProducts", JSON.stringify(cart));
-                                    }}>+</button><button className="btn btn-warning m-2" onClick={() => {
-                                        setShow(true);
-                                        setColorMsg("danger");
-                                        if (localStorage.getItem("cartProducts")) {
-                                            let cart = JSON.parse(localStorage.getItem("cartProducts"));
-                                            if (cart[service.serviceId]) {
-                                                cart[service.serviceId] -= 1;
-                                                setCount(count - 1);
-                                                localStorage.setItem("cartCount", count - 1);
-                                                console.log(cart[service.serviceId]);
-                                            }
-                                            localStorage.setItem("cartProducts", JSON.stringify(cart));
-                                        }
-                                    }}>-</button> */}
                                 </td>
-                                {/* <td><Form>
-      <Form.Check 
-        type="switch"
-        id="custom-switch"
-        label="make listing public"
-      /></Form>
-</td> */}
-                                {/* <td><button className="btn btn-success me-2" onClick={() => {
-                                    //Set isAvailable to true, to post listing
-                                    //This posts services for other users to see
-                                    s.available = true;
-                                    setService(s);
-                                    console.log(s);
-                                }}>List Service</button></td>
-                                <td><button className="btn btn-danger me-2" onClick={() => {
-                                    //Set isAvailable to true, to post listing
-                                    //This hides services for other users to not see
-                                    s.available = false;
-                                    setService(s);
-                                    console.log(s);
-                                }}>Unlist Service</button></td> */}
-                                {/* <td><Link to={`/services/delete/${service.serviceId}`} className="btn btn-danger m-2">Delete</Link></td> */}
-
                             </tr>
                         ))
                     }
-                    {/* {services.map(s =>
-                        <Service key={s.serviceId} service={s} count={count} setCount={setCount} handleEdit={handleEdit} handleDelete={handleDelete} />)} */}
                     {services.map(s => {
                         if (!localStorage.getItem("servicehm")) { localStorage.setItem("servicehm", JSON.stringify({})); }
                         let hm = JSON.parse(localStorage.getItem("servicehm"));
